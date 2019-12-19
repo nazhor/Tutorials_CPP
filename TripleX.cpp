06dcf11984d449da0ee181de2215c7b0bcce9804
David Bernad <david.bernad@scati.com>
2019-12-19 16:12:19 +0100

    Basic cpp

--- TripleX.cpp
+++ TripleX.cpp
@@ -4,5 +4,21 @@ int main()
 {
     std::cout << "You are a secret agent breaking into a secure server room" << std::endl;
     std::cout << "You need to enter the correct codes to continue..." << std::endl;
+
+    // int GuessA = 2;
+    // int GuessB = 5;
+    // int GuessC = 7;
+
+    // const int GuessSum = GuessA + GuessB + GuessC;
+    // const int GuessProduct = GuessA * GuessB * GuessC;
+
+    int GuessA;
+    int GuessB;
+    int GuessC;
+    std::cin >> GuessA;
+    std::cin >> GuessB;
+    std::cin >> GuessC;
+    std::cout << "You entered: " << GuessA  << GuessB << GuessC << std::endl;
+
     return 0;
 }
