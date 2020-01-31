1cf82ee636657d3c252b0bd93679a87df7685ec3
David <dbernad4220@gmail.com>
2020-01-31 19:58:08 +0100

    If Else

--- TripleX.cpp
+++ TripleX.cpp
@@ -27,7 +27,17 @@ int main()
     std::cout << "You entered: " << GuessA << GuessB << GuessC<< std::endl;
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
-    std::cout << "The sum is: " << GuessSum << " and the product is: " << GuessProduct << std::endl;
+    const int resultSum = 10;
+    const int resultProduct = 30;
+
+    if(GuessSum == resultSum && GuessProduct == resultProduct)
+    {
+        std::cout << "You win! The sum is: " << GuessSum << " and the product is: " << GuessProduct << std::endl;
+    }
+    else
+    {
+        std::cout << "Nop" << std::endl;
+    }
 
     return 0;
 }
