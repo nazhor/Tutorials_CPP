0088cffb6336f4de6b4feae32167ca5d026194c1
David <dbernad4220@gmail.com>
2020-01-31 19:48:13 +0100

    User input

---
+++ README.md
@@ -0,0 +1,3 @@
+# Unreal 4.22 C++ Developer:
+
+## https://www.gamedev.tv/courses/enrolled/675837
--- TripleX.cpp
+++ TripleX.cpp
@@ -5,20 +5,29 @@ int main()
     std::cout << "You are a secret agent breaking into a secure server room" << std::endl;
     std::cout << "You need to enter the correct codes to continue..." << std::endl;
 
+    // Part 1
     // int GuessA = 2;
     // int GuessB = 5;
     // int GuessC = 7;
-
     // const int GuessSum = GuessA + GuessB + GuessC;
     // const int GuessProduct = GuessA * GuessB * GuessC;
+    // int GuessA;
+    // int GuessB;
+    // int GuessC;
+    // std::cin >> GuessA;
+    // std::cin >> GuessB;
+    // std::cin >> GuessC;
+    // std::cout << "You entered: " << GuessA  << GuessB << GuessC << std::endl;
 
-    int GuessA;
-    int GuessB;
-    int GuessC;
+    // Part 2
+    int GuessA, GuessB, GuessC;
     std::cin >> GuessA;
     std::cin >> GuessB;
     std::cin >> GuessC;
-    std::cout << "You entered: " << GuessA  << GuessB << GuessC << std::endl;
+    std::cout << "You entered: " << GuessA << GuessB << GuessC<< std::endl;
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+    std::cout << "The sum is: " << GuessSum << " and the product is: " << GuessProduct << std::endl;
 
     return 0;
 }
